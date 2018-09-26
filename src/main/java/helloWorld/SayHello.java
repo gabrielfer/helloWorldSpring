44a46f4f3537461b1d4d1ee4cb8dfcb7d8c1bb43
Gabriel Fernandes da Silva <gabriel_fernandes@sicredi.com.br>
2018-09-26 18:07:14 -0300

    adding lucckyNumber

--- src/main/java/helloWorld/SayHello.java
+++ src/main/java/helloWorld/SayHello.java
@@ -3,12 +3,18 @@ package helloWorld;
 public class SayHello {
 
     private final String personName;
+    private int lucckyNumber;
 
-    public SayHello(String personName) {
+    public SayHello(String personName, int lucckyNumber) {
         this.personName = personName;
+        this.lucckyNumber = lucckyNumber;
     }
 
     public String getPersonName() {
         return personName;
+    }
+
+    public int getLucckyNumber() {
+        return lucckyNumber;
     }
 }
--- src/main/java/helloWorld/SayHelloController.java
+++ src/main/java/helloWorld/SayHelloController.java
@@ -4,15 +4,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Random;
+
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 @RestController
 public class SayHelloController {
 
     private static final String template = "Hello, %s!";
+    private final Random lucckyNumber = new Random();
 
     @RequestMapping(method = GET, value = "/sayHello")
     public SayHello sayHello(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return new SayHello(String.format(template, name));
+        return new SayHello(String.format(template, name), lucckyNumber.nextInt(999999));
     }
 }
